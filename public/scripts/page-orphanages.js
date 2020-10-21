0b17a01540ef95785bb89b201b6951171bf7d67c
Marcelo Santos <riquecelox462m@gmail.com>
2020-10-20 22:46:27 -0300

    commit adicionais

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,26 @@
+//creat map
+const map = L.map('mapid').setView([-1.4147594,-48.4453204], 15)
+
+//creat and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map)
+
+//creat icon 
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"</a>')
+
+//creat and add marker
+L.marker([-1.4147594,-48.4453204], {icon}).addTo(map)
+    .bindPopup(popup)
+    
