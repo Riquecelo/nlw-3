528f1b77d2eb282b15967656728793318c4b061b
Marcelo Santos <riquecelox462m@gmail.com>
2020-10-25 23:17:05 -0300

    finalizando dia 2 parte 3

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -24,5 +24,31 @@ const icon = L.icon({
 
 //creat and add marker
 L.marker([-1.4147594,-48.4453204], {icon}).addTo(map)
-    .bindPopup(popup)
-    
+
+/*image gallery*/
+
+function selectImage(event){
+    const button = event.currentTarget
+
+    console.log(button.children)
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button){
+        button.classList.remove("active")
+    }
+
+    //selecionar a image clicada
+    const images = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o containerde image
+    imageContainer.src = images.src
+
+
+    //adicionar a classe .active para o botao clicado
+    button.classList.add("active")
+
+}
